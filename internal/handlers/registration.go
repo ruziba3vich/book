a3264c093aaffa8e852af7a7f4979dbfe414fbd7
prodonik <rruzibayevich@gmail.com>
2024-04-17 19:20:46 +0500

    . . .

--- internal/handlers/registration.go
+++ internal/handlers/registration.go
@@ -14,13 +14,13 @@ func Register(c *gin.Context, db *sql.DB) {
 	var user app.User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
-        c.AbortWithError(http.StatusBadRequest, err)
-        return
-    }
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
 	token, err := service.Register(user, db)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": token})
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
